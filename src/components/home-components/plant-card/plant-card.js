f1ac43768478e9c7c9eaac42cbd9fde4a13601ee
Bryan <bryan.pilco@gmail.com>
2021-06-10 00:28:41 -0500

    Agregando servicio post de datos de campo

--- src/components/home-components/plant-card/plant-card.js
+++ src/components/home-components/plant-card/plant-card.js
@@ -3,11 +3,14 @@ import {Text, View} from 'react-native';
 import {cardStyles} from './plant-card-styles';
 import {DateTime} from 'luxon';
 import {TouchableOpacity} from 'react-native';
+import _ from 'lodash';
 export const PlantCard = ({item, navigation}) => {
   return (
     <TouchableOpacity
       onPress={() => {
         navigation.navigate('PlantScreen');
+        let id = _.get(item, 'item._id');
+        console.log(id);
       }}>
       <View style={cardStyles.container}>
         <Text>{item.item.commonName}</Text>
---
+++ src/services/campData-service.js
@@ -0,0 +1,25 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import {urlApi} from '../environment/variables';
+axios.defaults.baseURL = urlApi;
+
+export const campDataService = {
+  async registerCampData(formFields) {
+    const token = await getToken();
+    try {
+      const data = await axios.post(`${urlApi}/campData/`, formFields, {
+        headers: {Authorization: token},
+      });
+
+      return data.data.message;
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
+    }
+  },
+};
+
+const getToken = async () => {
+  const token = await AsyncStorage.getItem('token');
+  return token;
+};
